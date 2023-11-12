9335c6bc248689b24920bf4357f515d8a7c162ab
Dawid-Stankowski <dawid441@interia.pl>
2023-11-12 13:00:41 +0100

    1.0

--- src/Hello.java
+++ src/Hello.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class Hello {
+import java.util.Scanner;
+
+public class Hello
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Wprowadź tekst: ");
+
+        String wprowadzonyTekst = scanner.nextLine();
+
+        System.out.println("Wprowadzony tekst: " + wprowadzonyTekst);
+
+        scanner.close();
+    }
 }
